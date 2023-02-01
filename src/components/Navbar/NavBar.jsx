87023148012b9affda5e309f7957d6bf418e104e
Patrick Mazara <patrickmazara@hotmail.com>
2023-01-31 23:59:55 -0500

    edit NavBar component

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -3,12 +3,24 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   
   return (
+  <div>
     <header>
-      <Link to="/">PATRICK MAZARA</Link>
-      <Link to="/contact"></Link>
-      <Link to="/resume"></Link>
-			<Link to="/projects"></Link>
+      <ul>
+        <li>
+          <Link to="/">PATRICK MAZARA</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/resume">Resume</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>  
+      </ul> 
     </header>
+  </div>
   );
 };
 
