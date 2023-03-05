6692c10388862d3fc1c95257a8685266d23cd2c7
Patrick Mazara <patrickmazara@hotmail.com>
2023-03-04 21:49:00 -0500

    implement onClick function for menu on mobile view

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import Reac, { useState } from "react";
 import "./Header.css"
 
+
 const Header = () => {
+    const[Toggle, showMenu] = useState(false)
+    
+
   return (
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">Mazara</a>
+
+
         <div className="nav__menu">
           <ul className="nav__list grid">
             <li className="nav__item">
@@ -15,7 +21,7 @@ const Header = () => {
               </a>
             </li>
 
-            <li className="nav__item">
+            <li className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
               <a href="#about" className="nav__link">
                 <i className="uil uil-user nav__icon"></i> 
                 About
@@ -51,10 +57,10 @@ const Header = () => {
             </li>
           </ul>
 
-          <i className="uil uil-times nav__close"></i>
+          <i className="uil uil-times nav__close"  onClick={() => showMenu(!Toggle)}></i>
         </div>
 
-        <div className="nav__toggle">
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
           <i className="uil uil-apps"></i>
         </div>
       </nav>
