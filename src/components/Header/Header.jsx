ef66257fd538436f0f261e4dcd5b24c3c1b59cd0
Patrick Mazara <patrickmazara@hotmail.com>
2023-03-04 20:40:20 -0500

    add nav bar in Header, make edits to list properties

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import "./Header.css"
 
 const Header = () => {
   return (
-    <div>
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Mazara</a>
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href=" " className="nav__link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
 
-    </div>
+            <li className="nav__item">
+              <a href=" " className="nav__link">
+                <i className="uil uil-user nav__icon"></i>
+              </a>
+
+              <li className="nav__item">
+              <a href=" " className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href=" " className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href=" " className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href=" " className="nav__link">
+                <i className="uil uil- nav__icon"></i>
+              </a>
+            </li>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   )
 }
 
