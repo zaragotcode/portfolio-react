227e87fb21aec04054d737299e0f8e5c7ceb052d
Patrick Mazara <patrickmazara@hotmail.com>
2023-03-04 21:34:22 -0500

    add media query styling for nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,41 +9,53 @@ const Header = () => {
         <div className="nav__menu">
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href=" " className="nav__link">
-                <i className="uil uil-estate nav__icon"></i> Home
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> 
+                Home
               </a>
             </li>
 
             <li className="nav__item">
-              <a href=" " className="nav__link">
-                <i className="uil uil-user nav__icon"></i>
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> 
+                About
               </a>
+            </li>
 
               <li className="nav__item">
-              <a href=" " className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i>
+                Skills
               </a>
             </li>
 
             <li className="nav__item">
-              <a href=" " className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#servies" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i>
+                Services
               </a>
             </li>
 
             <li className="nav__item">
-              <a href=" " className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i>
+                Portfolio
               </a>
             </li>
 
             <li className="nav__item">
-              <a href=" " className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i>
+                Contact
               </a>
-            </li>
             </li>
           </ul>
+
+          <i className="uil uil-times nav__close"></i>
+        </div>
+
+        <div className="nav__toggle">
+          <i className="uil uil-apps"></i>
         </div>
       </nav>
     </header>
