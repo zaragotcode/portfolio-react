edeea5b3ba84d280595a874ddc71b1118d746ed8
Patrick Mazara <patrickmazara@hotmail.com>
2023-03-06 06:36:30 -0500

    complete About component, create Skills component and css files

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       
       <span className="section__subtitle">My Introduction</span>
     
-      <div className="about__containter container grid">
+      <div className="about__container container grid">
         <img src={AboutImg} alt="" className="about__img" />
         <div className="about__data">
           <Info />
--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -4,18 +4,21 @@ const Info = () => {
   return (
     <div className="about__info grid">
       <div className="about__box">
-        <h3 className="about__title"></h3>
-        <span className="about__subtitle"></span>
+        <i className="bx bx-award about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">8 Years</span>
       </div>
 
       <div className="about__box">
-        <h3 className="about__title"></h3>
-        <span className="about__subtitle"></span>
+        <i className="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">48+ Projects</span>
       </div>
 
       <div className="about__box">
-        <h3 className="about__title"></h3>
-        <span className="about__subtitle"></span>
+        <i className="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
       </div>
     </div>
   )
