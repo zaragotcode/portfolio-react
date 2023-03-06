cea277c5a31fe35023c38083e0cf658387bca83a
Patrick Mazara <patrickmazara@hotmail.com>
2023-03-06 09:53:44 -0500

    create Contact component and styling files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
+import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import ScrollUp from './components/ScrollUp/ScrollUp';
 
@@ -19,6 +20,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Contact />
     </main>
 
     <Footer />
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section className="contact section" id="contact">
+      <h2 className="section__title">...</h2>
+      <span className="section__title">...</span>
+
+    </section>
+  )
+}
+
+export default Contact
