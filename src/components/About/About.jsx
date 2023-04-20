11b567e0dc1bd91043378ff8f20316ecd19ca2cb
Patrick Mazara <patrickmazara@hotmail.com>
2023-04-20 17:14:32 -0400

    remove about photo, change header photo

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <h2 className="section__title">About Patrick</h2>
           
       <div className="about__container container grid">
-        <img src={AboutImg} alt="" className="about__img" />
+        {/* <img src={AboutImg} alt="" className="about__img" /> */}
         <div className="about__data">
         
           <p className="about__description">
